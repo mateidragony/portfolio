edc170ab208dc25d8ac41ece6cd2bc6288228ea9
Matei Cloteaux <matei.cloteaux@gmail.com>
2025-11-13 19:45:52 -0500

    update firefox links

--- extra/firefox/script.js
+++ extra/firefox/script.js
@@ -7,9 +7,9 @@ const links = {
 	'onedrive' : ['https://indiana-my.sharepoint.com/my'],
 	'github' : ['https://github.com/'],
 	'gmail' : ['https://mail.google.com/mail/u/0/#inbox'],
-	'house' : ['https://p-auth.duke-energy.com/my-account/dashboard',
+	'house' : ['https://www.duke-energy.com/home/billing/pay-bill',
 			   'https://myaccount.centerpointenergy.com',
-			   'https://esuite.bloomington.in.gov/eSuite.Utilities/AccountSummary/',
+			   'https://cityofbloomingtonin.municipalonlinepayments.com/cityofbloomingtonin/utilities',
 			   'https://indiana-my.sharepoint.com/:x:/r/personal/mcloteau_iu_edu/_layouts/15/Doc.aspx?sourcedoc=%7B3BE5C5E1-734A-4A0B-823A-955E24B7BCAD%7D&file=Excel-Tactics-Shared-Expense-Calculator-Extended.xlsx'],
 	'teams' : ['https://teams.microsoft.com']
 }
